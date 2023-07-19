4cce66d4dfb234820e9b88d97df9d844ed31610b
Nobuhiko Tobita <nobu1006@gmail.com>
2023-07-19 16:29:10 +0900

    warでサーバーにデプロイできるように修正

--- src/main/java/com/example/Ec202110cApplication.java
+++ src/main/java/com/example/Ec202110cApplication.java
@@ -3,12 +3,18 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
-public class Ec202110cApplication {
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class Ec202110cApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ec202110cApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Ec202110cApplication.class);
+	}
 }
